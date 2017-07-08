62cc6b326b6750e12e5e5c7cc13d14ff6b713fcc
Maxim Shafarostov <capestiff@ya.ru>
2017-07-08 16:08:41 +0300

    try...except refactoring

--- get_html.py
+++ get_html.py
@@ -3,15 +3,16 @@ import requests
 def get_html(url, params=None):
     user_agent = {'User-agent': 'Mozilla/5.0'}
 
+    result = requests.Response
+    result.status_code = None
+
     try:
         result = requests.get(url=url, headers=user_agent, params=params)
-
-        result.raise_for_status()
-
-        if result.status_code == requests.codes.ok:
-            return result.text
-        else:
-            print('Can\'t get html by url.')
     except requests.RequestException as error:
         print(error)
-        return '<html><head></head><body><table><tr><td></td></tr></table></body></html>'
+
+    if result.status_code == requests.codes.ok:
+        return result.text
+    else:
+        print('Something goes wrong.')
+        return None
