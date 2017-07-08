cd93dfa7261311fddda010f8c77640682913d9f2
Maxim Shafarostov <capestiff@ya.ru>
2017-07-08 10:55:04 +0300

    Errors processing added.

--- get_html.py
+++ get_html.py
@@ -1,9 +1,17 @@
 import requests
 
-def get_html(url):
+def get_html(url, params=None):
     user_agent = {'User-agent': 'Mozilla/5.0'}
-    result = requests.get(url=url, headers=user_agent)
-    if result.status_code == requests.codes.ok:
-        return result.text
-    else:
-        print('Can\'t get html by url.')
+
+    try:
+        result = requests.get(url=url, headers=user_agent, params=params)
+
+        result.raise_for_status()
+
+        if result.status_code == requests.codes.ok:
+            return result.text
+        else:
+            print('Can\'t get html by url.')
+    except requests.RequestException as error:
+        print(error)
+        return '<html><head></head><body><table><tr><td></td></tr></table></body></html>'
